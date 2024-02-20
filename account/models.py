3bd4b44158e32f34e14b082680c0701044f796d9
hcoagithub <hectocoa.14@gmail.com>
2024-02-19 22:02:26 -0300

    Agregar mmodelo Usuario en models.py

--- account/models.py
+++ account/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+class Usuario(AbstractUser):
+    rut= models.CharField(max_length=9, unique=True)
+    celular= models.CharField(max_length=9, unique=True)
+    direccion= models.CharField(max_length=500)
+    foto= models.ImageField(upload_to='media/usuario')
+
+    def __str__(self):
+        return self.username
 
