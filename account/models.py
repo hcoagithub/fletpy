d2ed8483f414d2761e9256b67da9b1ecafc561b6
hcoagithub <hectocoa.14@gmail.com>
2024-02-19 22:11:55 -0300

    Agregar UsuarioForm a forms.py

--- account/models.py
+++ account/models.py
@@ -9,6 +9,8 @@ class Usuario(AbstractUser):
     direccion= models.CharField(max_length=500)
     foto= models.ImageField(upload_to='media/usuario')
 
+    is_conductor= models.BooleanField(default=False)
+
     def __str__(self):
         return self.username
 
