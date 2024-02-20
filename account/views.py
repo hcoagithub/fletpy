a5f40945e4154bc15dc300c49a9e1564cbac047c
hcoagithub <hectocoa.14@gmail.com>
2024-02-19 22:19:53 -0300

    Crear la funcion de registro de usuario en views.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+
+from .forms import *
 
 # Create your views here.
+
+def user_create(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('account:login')
+    else:
+        form = UsuarioForm()
+    return render(request, 'account/registro.html', {'form': form})
+
+
